0b836317252a1ed9ad18a6a3bf2b669905ff2294
Tony Blum <tonyblum@me.com>
2026-09-20 11:26:13 -0600

    Harden Radio WOLO Traffic index migration

--- prisma/migrations/20260920170000_radio_traffic_analytics/migration.sql
+++ prisma/migrations/20260920170000_radio_traffic_analytics/migration.sql
@@ -7,7 +7,7 @@ ALTER TABLE "radio_listener_states"
   ADD COLUMN "last_interaction" VARCHAR(32),
   ADD COLUMN "sound_ever_on_at" TIMESTAMP(6);
 
-CREATE INDEX "ix_radio_listener_states_traffic_visitor_seen"
-  ON "radio_listener_states"("traffic_visitor_id", "last_seen_at");
+CREATE INDEX "ix_radio_listener_states_traffic_visitor"
+  ON "radio_listener_states"("traffic_visitor_id");
 
 COMMIT;
